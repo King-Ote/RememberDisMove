1177c5b6e497ba797d1ade07ccf03e7f155c0288
King-Ote <notreallynico@hotmail.com>
2017-03-30 14:14:28 +1000

    First Commit
    First commit. Working on message list emptying at max size. Building
    'reveal' function to .show() and .animate()

---
+++ RememberDisMove.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/embers')
+def embers_route():
+    return render_template("embers.html")
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=5000, debug=True)
