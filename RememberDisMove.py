185d6b75caed89802408f58dc64dfef98b81e95a
King-Ote <notreallynico@hotmail.com>
2017-10-13 08:33:20 +1000

    Added WorstWizard
    Dicked about getting the WorstWizard uploaded. It does not work, I know
    not why

--- RememberDisMove.py
+++ RememberDisMove.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, render_template_string
 
 app = Flask(__name__)
 
@@ -7,9 +7,27 @@ app = Flask(__name__)
 def hello_world():
     return 'CURRENTLY UNDER CONSTRUCTION'
 
-@app.route('/embers')
+
+@app.route('/generators')
+def generators_route():
+    # Should probably get available generators out of a database and build links to them to zepto into the page?
+    return render_template("generators.html")
+
+
+@app.route('/games')
+def games_route():
+    return render_template("games.html")
+
+
+@app.route('/games/embers')
 def embers_route():
     return render_template("embers.html")
+
+@app.route('/games/worstwizard')
+def worstwizard_route():
+    return render_template("worstwizard.html")
+
+
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=5000, debug=True)
